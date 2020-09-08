014fbea41429b99abde70bd14c211d6cc7901ac4
HumbleNarcissus <maciek.tarach06@gmail.com>
2020-09-08 22:56:56 +0200

    show statistics charts

---
+++ ml-frontend/components/DoughnutChart.vue
@@ -0,0 +1,31 @@
+<script>
+import { Doughnut } from "vue-chartjs";
+
+export default {
+  extends: Doughnut,
+  props: {
+    labels: {
+      type: Array,
+      default: () => []
+    },
+    datasets: {
+      type: Array,
+      default: () => []
+    }
+  },
+  mounted() {
+    this.renderChart({
+      labels: this.labels,
+      datasets: this.datasets
+    });
+  },
+  watch: {
+    datasets() {
+      this.renderChart({
+        labels: this.labels,
+        datasets: this.datasets
+      });
+    }
+  }
+};
+</script>
--- ml-frontend/components/MovieDialog.vue
+++ ml-frontend/components/MovieDialog.vue
@@ -3,15 +3,21 @@
     <v-card>
       <v-card-title>{{ movie.title }}</v-card-title>
       <comments-table :comments="movie.comments" />
+      <doughnut-chart
+        :labels="['Positive', 'Negative']"
+        :datasets="createDataSets()"
+      />
     </v-card>
   </v-dialog>
 </template>
 
 <script>
-import CommentsTable from '~/components/CommentsTable.vue'
+import CommentsTable from "~/components/CommentsTable.vue";
+import DoughnutChart from "~/components/DoughnutChart.vue";
 export default {
   compontenst: {
-    CommentsTable
+    CommentsTable,
+    DoughnutChart
   },
   props: {
     movie: {
@@ -22,11 +28,17 @@ export default {
   data() {
     return {
       dialog: false
-    }
+    };
   },
   methods: {
     openDialog() {
-      this.dialog = true
+      this.dialog = true;
+    },
+    createDataSets() {
+      const { comments = [] } = this.movie;
+      const positive = comments.filter(item => item.positive === "Positive");
+      const negative = comments.filter(item => item.positive === "Negative");
+      return [{ data: [positive.length, negative.length], backgroundColor: ['green', 'red'] }];
     }
   }
 };
