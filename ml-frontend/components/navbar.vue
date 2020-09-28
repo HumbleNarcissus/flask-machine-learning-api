34a9f11498bb6af237c0ac0e1c517d425ab17892
HumbleNarcissus <maciek.tarach06@gmail.com>
2020-09-28 22:44:50 +0200

    change title

--- ml-frontend/components/navbar.vue
+++ ml-frontend/components/navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-app-bar dense dark>
-      <v-toolbar-title>ML movie comment check</v-toolbar-title>
+      <v-toolbar-title>Check comments with machine learning</v-toolbar-title>
       <v-spacer />
       <Dialog />
     </v-app-bar>
